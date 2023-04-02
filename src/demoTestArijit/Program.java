269f642592195dc5f4016f821d41284bf33979f8
Sawon_bhowmik <sawonbhowmik@gmail.com>
2023-04-02 12:47:02 +0530

    Method for Reversing a List

--- src/demoTestArijit/Program.java
+++ src/demoTestArijit/Program.java
@@ -14,12 +14,30 @@ public class Program {
 			System.out.println("Enter the value at i : "+ i);
 			num.add(sc.nextInt());
 		}
-		
 		System.out.println(num);
+		sc.close();
 		//Print this list in reverse order//
-		//Print 4 th elemet of the list//
+		//[67, 8, 0, 45, 23]
+		System.out.println("Printing the List in reverse Order: ");
+		System.out.println(reverseList(num, len));
+		//Print 4 th elemet of the list// 
+		System.out.println(num.get(3));
+		
 		//Write a code to print ith fibonacci no//
 		//Write a code to reverse a string//
+	}
+
+	private static List<Integer> reverseList(List<Integer> num, int n) {
+		int i = 0;
+		int j = n-1;
+		while (i < j) {
+			int temp = num.get(i);
+			num.set(i, num.get(j));
+			num.set(j, temp);
+			i ++;
+			j --;
+		}
+		return num;
 	}
 
 }
